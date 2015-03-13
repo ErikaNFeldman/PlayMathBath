386049ac81f84ecc428d9d99c79e737d4be60c50
ErikaNFeldman <feldmanerika@gmail.com>
2015-03-12 20:33:21 -0700

    displays top 1/4 of screen the segue btwn screens work

---
+++ PlayMathBath/PlayMathBath/problemViewController.swift
@@ -0,0 +1,29 @@
+//
+//  problemViewController.swift
+//  PlayMathBath
+//
+//  Created by Erika Feldman on 3/11/15.
+//  Copyright (c) 2015 Early Learning & Play. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class problemViewController: UIViewController {
+
+    var buttonValueOne = 1
+    var buttonValueTwo = 2
+ 
+    
+    //var resultLabel = buttonValueOne + buttonValueTwo
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    //?override fun prepareForSeque(segue: UIStoryboardSegue, sender:AnyObject?){
+    
+    //calculateResult
+    //}
+}
---
+++ PlayMathBath/PlayMathBath/resultViewController.swift
@@ -0,0 +1,15 @@
+//
+//  resultViewController.swift
+//  PlayMathBath
+//
+//  Created by Erika Feldman on 3/11/15.
+//  Copyright (c) 2015 Early Learning & Play. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class resultViewController: UIViewController {
+    
+}
